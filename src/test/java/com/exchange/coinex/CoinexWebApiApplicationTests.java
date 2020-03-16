dac604c0551355538f61e3e356f738afc2318e42
wangbinzero <wangbbhtt@gmail.com>
2020-03-16 12:41:07 +0800

    init

---
+++ src/test/java/com/exchange/coinex/CoinexWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchange.coinex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinexWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
